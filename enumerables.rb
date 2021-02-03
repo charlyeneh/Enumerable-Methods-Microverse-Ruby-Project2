03f8015ac12ef4025e719fb4f4944c1c601a3560
angelacuahutle <angela.cuahutle@gmail.com>
2021-02-03 10:06:42 -0600

    add second method each with index

--- enumerables.rb
+++ enumerables.rb
@@ -1,15 +1,22 @@
-module Enumerable
-  def ca_each
-    return to_enum unless block_given?
-    example_array = is_a?(Array) ? self : to_a
-    position_in_array = 0 
-    while position_in_array < example_array.length
-      yield example_array[position_in_array]
-      position_in_array += 1
-    end
-      example_array
-  end
-end
-  example_array = [2,3,59,99,203,202,22]
-  example_array.ca_each {|number| puts number}
+# module Enumerable
+#   def ca_each
+#     return to_enum unless block_given?
+#     example_array = is_a?(Array) ? self : to_a
+#     position_in_array = 0 
+#     while position_in_array < example_array.length
+#       yield example_array[position_in_array]
+#       position_in_array += 1
+#     end
+#       example_array
+#   end
+# end
+# example_array = [2,3,59,99,203,202,22]
+# example_array.ca_each {|number| puts number}
 
+  def ca_each_with_index
+    hash = hash.size
+    %w(four seven ten).ca_each_with_index {|place, index|
+    hash[place] = index 
+    }
+    hash
+    #=>
