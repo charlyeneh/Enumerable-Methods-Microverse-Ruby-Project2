f12987addde247d6dc229a5aa3a7fbf6834b4b12
angelacuahutle <angela.cuahutle@gmail.com>
2021-02-04 15:05:58 -0600

    add my each index

--- enumerables.rb
+++ enumerables.rb
@@ -12,11 +12,25 @@
 # end
 # example_array = [2,3,59,99,203,202,22]
 # example_array.ca_each {|number| puts number}
-
   def ca_each_with_index
-    hash = hash.size
-    %w(four seven ten).ca_each_with_index {|place, index|
-    hash[place] = index 
-    }
-    hash
+    return to_enum unless block_given?
+    example_array = is_a? (Array) ? self : to_a
+    i = 0 
+    while i < .length
+      yield(self[i], i)
+      i += 1
+    end 
+    self 
+  end 
+
+    #hash = hash.size
+    #%w(four seven ten).ca_each_with_index {|place, index|
+    #hash[place] = index 
+    #}
+    #hash
     #=>
+
+  def ca_select
+    return to_enum (:ca_select) unless block_given?
+
+   
