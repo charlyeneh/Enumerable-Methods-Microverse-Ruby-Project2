2041f7e0d9957e2eb53860bf18e80b58177bccd6
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2021-02-13 00:56:29 +0100

    fix some more linters errors

--- enumerables.rb
+++ enumerables.rb
@@ -1,11 +1,12 @@
 module Enumerable
   def my_each
     return to_enum unless block_given?
-     example_array = is_a?(Array) ? self : to_a
-     position_in_array = 0 
+
+    example_array = is_a?(Array) ? self : to_a
+    position_in_array = 0
       while position_in_array < example_array.length
         yield example_array[position_in_array]
-        position_in_array += 1
+      position_in_array += 1
       end
        example_array
    end
