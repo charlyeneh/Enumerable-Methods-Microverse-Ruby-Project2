189657bd75e2bd53decb085449e928e45a9cbf49
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2021-02-12 12:29:47 +0100

    fix indentation and adds my inject

--- enumerables.rb
+++ enumerables.rb
@@ -1,15 +1,18 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Style/CaseEquality
 module Enumerable
-   def my_each
-     return to_enum unless block_given?
+  def my_each
+    return to_enum unless block_given?
      example_array = is_a?(Array) ? self : to_a
      position_in_array = 0 
-     while position_in_array < example_array.length
-       yield example_array[position_in_array]
-       position_in_array += 1
-     end
+      while position_in_array < example_array.length
+        yield example_array[position_in_array]
+        position_in_array += 1
+      end
        example_array
    end
-
+  end
   example_array = [2,3,59,99,203,202,22]
   example_array.my_each {|number| puts number}
 
@@ -22,15 +25,8 @@ module Enumerable
       i += 1
     end 
     self 
-  end 
-
-    #hash = hash.size
-    #%w(four seven ten).my_each_with_index {|place, index|
-    #hash[place] = index 
-    #}
-    #hash
-    #=>
-
+    end 
+  end
   def my_select
     return to_enum(:my_select) unless block_given?
 
@@ -38,8 +34,7 @@ module Enumerable
     my_each { |element| new_arr << element if yield(element) }
     new_arr
   end
-
-def my_all?(*args)
+  def my_all?(*args)
     if !args[0].nil?
       my_each { |element| return false unless args[0] === element }
     elsif block_given?
@@ -49,7 +44,6 @@ def my_all?(*args)
     end
     true
   end
-
   def my_any?(*args)
     if !args[0].nil?
       my_each { |element| return true if args[0] === element }
@@ -60,22 +54,20 @@ def my_all?(*args)
     end
     false
   end
-
   def my_none?(args = nil, &block)
     !my_any?(args, &block)
   end
-  
   def my_count (arg = nil)
     c = 0  
     if block_given?
       my_each { |x| c +=1 if yield(x) }
-    elsif !block && arg.nil?
-      c = length
-    else c = my_select { |x| x == arg }. length
+      elsif !block && arg.nil?
+        c = length
+      else c = my_select { |x| x == arg }. length
+      end
+      c
     end
-    c 
   end
-
   def my_map (proc = nil)
     return to_enum(:my_map) unless block_given? || !proc.nil?
 
@@ -87,10 +79,26 @@ def my_all?(*args)
       end
      arr 
     end
-
-    def my_inject(total = nil, n = nil)
-      if total.is_a?(symbol)
-        n = total
-        total = nil
-      end 
-
+  end
+  def my_inject(*arg)
+    arg[0].is_a?(Integer) ? initial = arg[0] : symbol = arg[0]
+    if initial && !arg[1].is_a?(Integer)
+      symbol = arg[1]
+    elsif arg.nil?
+      initial = self[0]
+    end
+    result = initial
+    if symbol
+      my_each { |value| result = result ? result.send(symbol, value) : value }
+    else
+      my_each { |value| result = result ? yield(result, value) : value }
+    end
+    result
+  end
+  def multiply_els(arr)
+  arr.my_inject(:*)
+  end
+end
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Style/CaseEquality
