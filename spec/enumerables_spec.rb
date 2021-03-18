7b222fec4b4146fe417fa24822f4bb4fa4bcc21e
Promise Johnson <chiemelapromise30@gmail.com>
2021-03-18 01:07:46 +0000

    Add the second rspec for the method my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,15 +1,17 @@
 require_relative '../enumerables'
 require_relative './spec_helper'
 
-#   describe "#my_each"
-#     it "should return each item in an array" do
-#         expect([2, 4, 6, 8, 10].my_each { |number| p "Element number #{number}" ) to eql("Element number #{number}")
-#     end
-#   end
-
+example_array = [2, 3, 59, 99, 203, 202, 22]
 describe "#my_each" do
-    example_array = [2, 3, 59, 99, 203, 202, 22]
     it 'returns the iteration of the array' do
       expect(example_array.my_each { |number| p "Element number #{number}" }).to eql(example_array.each {|single| p "Element number #{single}"})
     end
-  end
+end
+
+describe "#my_each_with_index" do
+    it "describes a members of an array with their index too." do
+        expect(example_array.my_each_with_index { |number, index| p "Element number #{number} and #{index}"}).to eql(example_array.each_with_index{ |number, index| p "Element number #{number} and #{index}"})
+    end
+    
+end
+
