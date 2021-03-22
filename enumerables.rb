bd0b3134bd4c7274a89987562647a96cd4dd1346
craigs40 <craig.shay40@gmail.com>
2021-03-22 13:15:52 -0400

    Remove rubocop config

--- enumerables.rb
+++ enumerables.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 module Enumerable
   def my_each
     block_given? ? size.times { |i| yield(to_a[i]) } : (return to_enum)
@@ -89,4 +88,3 @@ end
 def multiply_els(arr)
   arr.my_inject(:*)
 end
-# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
